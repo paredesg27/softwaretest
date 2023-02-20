b6472713f44821b44bbe70fc814745088f63ee46
George Paredes <119899081+paredesg27@users.noreply.github.com>
2023-02-20 13:50:08 -0500

    Update Controller.php

--- lab3/Controller.php
+++ lab3/Controller.php
@@ -1,290 +1,227 @@
 <?php
-require_once 'Main.php';
+declare(strict_types=1);
 
-use PHPUnit\Framework\TestCase;
-
-class ControllerTest extends TestCase
-{
-    private $model;
-    private $view;
-    private $sut;
+class YahtzeeController {
     
-    public function setUp() :void {
-        $d = new YahtzeeDice();
-        $this->model = new Yahtzee($d);
-        $this->view = $this->createStub(YahtzeeView::class);
-        $this->sut = new YahtzeeController($this->model, $this->view);
-    } 
-    // @covers get_model()
-    public function test_get_model(){
-        $result = $this->sut->get_model();
-        $this->assertNotNull($result);
-    }
-    // @covers get_view()
-    public function test_get_view(){
-        $result = $this->sut->get_view();
-        $this->assertNotNull($result);
-    }
-    // @covers get_possibile_categories 
-    public function test_get_possible_categories(){
-        $result = $this->sut->get_possible_categories();
-        $this->assertNotNull($result);
-    }
-    public function test_get_possible_categories_ones(){
-       $stub = $this->createStub(Yahtzee::class);
-        // Configure the stub.
-      $stub->method('get_kept_dice')->willReturn(array(1,1,1,1,1));
-        $dice = $stub->get_kept_dice();
-        $this->model->update_scorecard("ones", array_sum($dice));
-        $result = $this->sut->get_possible_categories();
-        
-        $this->assertEquals($result['ones'], 5);
-    }
-    public function test_get_possible_categories_twos(){
-       $stub = $this->createStub(Yahtzee::class);
-        // Configure the stub.
-      $stub->method('get_kept_dice')->willReturn(array(1,1,1,1,1));
-        $dice = $stub->get_kept_dice();
-        $this->model->update_scorecard("twos", array_sum($dice));
-        $result = $this->sut->get_possible_categories();
-        
-        $this->assertEquals($result['twos'], 5);
-    }
-    public function test_get_possible_categories_threes(){
-       $stub = $this->createStub(Yahtzee::class);
-        // Configure the stub.
-      $stub->method('get_kept_dice')->willReturn(array(1,1,1,1,1));
-        $dice = $stub->get_kept_dice();
-        $this->model->update_scorecard("threes", array_sum($dice));
-        $result = $this->sut->get_possible_categories();
-        
-        $this->assertEquals($result['threes'], 5);
-    }
-    public function test_get_possible_categories_fours(){
-       $stub = $this->createStub(Yahtzee::class);
-        // Configure the stub.
-      $stub->method('get_kept_dice')->willReturn(array(1,1,1,1,1));
-        $dice = $stub->get_kept_dice();
-        $this->model->update_scorecard("fours", array_sum($dice));
-        $result = $this->sut->get_possible_categories();
-        
-        $this->assertEquals($result['fours'], 5);
-    }
-    public function test_get_possible_categories_fives(){
-       $stub = $this->createStub(Yahtzee::class);
-        // Configure the stub.
-      $stub->method('get_kept_dice')->willReturn(array(1,1,1,1,1));
-        $dice = $stub->get_kept_dice();
-        $this->model->update_scorecard("fives", array_sum($dice));
-        $result = $this->sut->get_possible_categories();
-        
-        $this->assertEquals($result['fives'], 5);
-    }
-    public function test_get_possible_categories_sixes(){
-       $stub = $this->createStub(Yahtzee::class);
-        // Configure the stub.
-      $stub->method('get_kept_dice')->willReturn(array(1,1,1,1,1));
-        $dice = $stub->get_kept_dice();
-        $this->model->update_scorecard("sixes", array_sum($dice));
-        $result = $this->sut->get_possible_categories();
-        
-        $this->assertEquals($result['sixes'], 5);
-    } 
-    public function test_get_possible_categories_three_of_a_kind(){
-       $stub = $this->createStub(Yahtzee::class);
-        // Configure the stub.
-      $stub->method('get_kept_dice')->willReturn(array(1,1,1,1,1));
-        $dice = $stub->get_kept_dice();
-        $this->model->update_scorecard("three_of_a_kind", array_sum($dice));
-        $result = $this->sut->get_possible_categories();
-        
-        $this->assertEquals($result['three_of_a_kind'], 5);
-    }
-    public function test_get_possible_categories_four_kind(){
-       $stub = $this->createStub(Yahtzee::class);
-        // Configure the stub.
-      $stub->method('get_kept_dice')->willReturn(array(1,1,1,1,1));
-        $dice = $stub->get_kept_dice();
-        $this->model->update_scorecard("four_of_a_kind", array_sum($dice));
-        $result = $this->sut->get_possible_categories();
-        
-        $this->assertEquals($result['four_of_a_kind'], 5);
-    }
-    public function test_get_possible_categories_full_house(){
-       $stub = $this->createStub(Yahtzee::class);
-        // Configure the stub.
-      $stub->method('get_kept_dice')->willReturn(array(1,1,1,1,1));
-        $dice = $stub->get_kept_dice();
-        $this->model->update_scorecard("full_house", array_sum($dice));
-        $result = $this->sut->get_possible_categories();
-        
-        $this->assertEquals($result['full_house'], 5);
-    }
-    public function test_get_possible_categories_small_straight(){
-       $stub = $this->createStub(Yahtzee::class);
-        // Configure the stub.
-      $stub->method('get_kept_dice')->willReturn(array(1,1,1,1,1));
-        $dice = $stub->get_kept_dice();
-        $this->model->update_scorecard("small_straight", array_sum($dice));
-        $result = $this->sut->get_possible_categories();
-        
-        $this->assertEquals($result['small_straight'], 5);
-    }
-    public function test_get_possible_categories_large_straight(){
-       $stub = $this->createStub(Yahtzee::class);
-        // Configure the stub.
-      $stub->method('get_kept_dice')->willReturn(array(1,1,1,1,1));
-        $dice = $stub->get_kept_dice();
-        $this->model->update_scorecard("large_straight", array_sum($dice));
-        $result = $this->sut->get_possible_categories();
-        
-        $this->assertEquals($result['large_straight'], 5);
-    }
-    public function test_get_possible_categories_chance(){
-        $stub = $this->createStub(Yahtzee::class);
-        // Configure the stub.
-      $stub->method('get_kept_dice')->willReturn(array(1,1,1,1,1));
-        $dice = $stub->get_kept_dice();
-        $this->model->update_scorecard("chance", array_sum($dice));
-        $result = $this->sut->get_possible_categories();
-        
-        $this->assertEquals($result['chance'], 5);
-    }
-    public function test_get_possible_categories_yahtzee(){
-        $stub = $this->createStub(Yahtzee::class);
-        // Configure the stub.
-        $stub->method('get_kept_dice')->willReturn(array(1,1,1,1,1));
-        $dice = $stub->get_kept_dice();
-        $this->model->update_scorecard("yahtzee", array_sum($dice));
-        $result = $this->sut->get_possible_categories();
-        
-        $this->assertEquals($result['yahtzee'], 5);
-    }
-    // @cover process_score_input
-     public function test_process_score_input_q(){
-        $result = $this->sut->process_score_input("q");
-        $this->assertEquals(-1, $result);
-    }
-     // @cover process_score_input
-    public function test_process_score_input_exit(){
-        $result = $this->sut->process_score_input("exit");
-        $this->assertEquals(-1, $result);
-    }
-
-    public function test_process_score_input_ones(){
-        $this->model = $this->createStub(Yahtzee::class);
-        $stub = $this->model;
-        $stub->method('get_kept_dice')->willReturn(array(1,1,1,1,1));
-        
-        $result = $this->sut->process_score_input("ones");
-        $this->assertEquals($result, 0);
-    }
-     // @cover process_score_input
-    public function test_process_score_input_invalid_category(){
-        $result = $this->sut->process_score_input("aple");
-        $this->assertEquals($result, -2);
-    }
-     // @cover process_keep_input
-    public function test_process_keep_input_exit() {
-        //set up (done in fixture)
-        
-        //call
-        $result = $this->sut->process_keep_input("exit");
-        //check
-        $this->assertEquals(-1, $result);
+    private Yahtzee $model;
+    private YahtzeeView $view;
     
+  
+    
+    public function __construct(Yahtzee $model, YahtzeeView $view){
+        $this->model = $model;
+        $this->view = $view;
     }
-    // @cover process_keep_input
-    public function test_process_keep_input_q() {
-        //set up (done in fixture)
-        $result = $this->sut->process_keep_input("q");
-        $this->assertEquals(-1, $result);
+    
+    public function get_model() : Yahtzee {
+        /*
+        get_model() : YahtzeeModel
+        > This method should return the model set by the constructor.
+        */
+        return $this->model;       
     }
-    // @cover process_keep_input
-    public function test_process_keep_input_do_nothing(){
-        //> If the line given is "none" "pass" or blank "", return -2.
-        $result = $this->sut->process_keep_input("none");
-        $this->assertEquals(-2, $result);
+    
+    public function get_view() : YahtzeeView {
+        /*
+        get_view() : YahtzeeView
+        > This method should return the view set by the constructor.
+        */
+        return $this->view;
     }
-    // @cover process_keep_input
-    public function test_process_keep_input_do_nothing1(){
-        //> If the line given is "none" "pass" or blank "", return -2.
-        $result = $this->sut->process_keep_input("pass");
-        $this->assertEquals(-2, $result);
+    
+    public function get_possible_categories() : array {
+        /*
+        get_possible_categories() : array
+        Returns an associative array.
+        > The keys are a valid category on the score card (one that is
+          currently NULL) and the values are what the score would be if the
+          user chose that category, based on the model's "kept_dice".
+        */
+        $rv = array();
+        $rv = $this->model->get_scorecard();
+        // echo(print_r($rv, true));
+        return $rv;
     }
-    // @cover process_keep_input
-    public function test_process_keep_input_do_nothing2(){
-        //> If the line given is "none" "pass" or blank "", return -2.
-        $result = $this->sut->process_keep_input("");
-        $this->assertEquals(-2, $result);
-    }
-    // @cover process_keep_input
-    public function test_process_keep_input_all(){
-        $this->sut->get_model()->roll(5);
-        $result = $this->sut->process_keep_input("all");
-        $this->assertEquals(0, $result);
-        $this->assertEquals(5, count($this->sut->get_model()->get_kept_dice()));
-    }
-    // @cover process_keep_input
-    public function test_process_keep_input_valid_index(){
-             $this->sut->get_model()->roll(5);
-            $result = $this->sut->process_keep_input("1");
-            $this->assertEquals(4, $result);
+    
+    public function process_score_input(string $line) : int {
+        /*
+        process_score_input(string: $line) : int
+        Returns an integer that denotes the status of the user input for when
+        they are choosing their score.
+        > If the line given is "exit" or "q", return -1.
+        - If an invalid category is given (meaning, one that is not in the 
+          scorecard's keys, it should:
+          > Update the first available category (NULL) to the score calculated
+            from the model's kept_dice attribute.
+          > Return -2, indicating an error occurred
+        > If a valid category is given, it should update that category to the
+          score calculated from the model's kept_dice attribute.
+        */ 
+    
+        //how to stub model's kept_dice attribute
+        // 
+
+        $scorecardValues = $this->model->get_scorecard();
+        $dice = $this->get_model->get_kept_dice();
+        $dice = array(1,1,1,1);
+        if($line == "q" || $line == "exit"){
+          return -1;
         }
-        // @cover process_keep_input
-    public function test_process_keep_input_throw_exception_invalid_index(){
-        $result = $this->sut->process_keep_input("ons");
-        $this->assertEquals(-2, $result);
-    }
-    // @cover process_keep_input
-    public function test_process_keep_input_throw_exception_too_many_dice(){
-        $result = $this->sut->process_keep_input("1 2 3 4 5 6");
-        $this->assertEquals(-2, $result);
-    }
-    // @cover handle_roll
-   public function test_handle_roll(){
-        $result = $this->sut->handle_roll();
-        $this->assertEquals($result, 0);
-    }
-      // @cover handle_roll
-    public function test_handle_roll_correct_user_strings(){
-        $stub = $this->view;
-        $stub->method('get_user_input')->willReturn("ones");
-        $result = $this->sut->handle_roll();
-        $this->assertEquals($result, 5);
-    }    
-     // @cover handle_roll
-    public function test_handle_roll_invalid_user_strings(){
-        $stub = $this->view;
-        $stub->method('get_user_input')->willReturn("ons");
-        $result = $this->sut->handle_roll();
-        $this->assertEquals($result, 0);
-    }
-    // @cover handle_roll
-    public function test_handle_roll_q_user_string(){
-        $stub = $this->view;
-        $stub->method('get_user_input')->willReturn("q");
-        $result = $this->sut->handle_roll();
-        $this->assertEquals($result, -1);
-    }
-         // @cover main_loop
-    public function test_main_loop(){
-        $result = $this->sut->main_loop();
-        $this->assertEquals($result, 0);
-    }
-     // @cover main_loop
-    public function test_main_loop_roll_q(){
-        $stub = $this->view;
-        $stub->method('get_user_input')->willReturn("q");
-        $result = $this->sut->main_loop();
-        $this->assertEquals($result, -1);
-    }
-     // @cover main_loop
-    public function test_main_loop_user_input_q(){
-        $stub = $this->view;
-    $stub->method('get_user_input')->willReturnOnConsecutiveCalls("all","q");
-    $result = $this->sut->main_loop();
-    $this->assertEquals($result, -1);
+        elseif(array_key_exists($line, $scorecardValues)){
+          //  $this->model->roll(5);
+          //  $dice = $this->get_model()->get_kept_dice();
+          // echo(strval(array_sum($dice)));
+          $this->model->update_scorecard($line,array_sum($dice));
+          echo(print_r($this->model->get_scorecard(), true));
+        }
+        else{
+          foreach($scorecardValues as $key=>$value){
+            if(is_null($scorecardValues[$key])){
+              $this->model->update_scorecard($key, 1);
+              break;
+            }
+          
+          }
+          // echo(print_r($this->model->get_scorecard(), true));
+          return -2;
+        }
+        
+        return 0;
     }
     
+    public function process_keep_input(string $line) : int {
+        /*
+        process_keep_input(string $line) : int
+        Processes the input from the user when determining which dice to keep.
+        > If the line given is "exit" or "q", return -1.
+        > If the line given is "all", immediately combine dice and return 0;
+        > If the line given is "none" "pass" or blank "", return -2.
+        - Otherwise, indexes are given
+          > Attempt to call keep_by_index with the given line, return the int
+            it returns, indicating remaining dice.
+          > If keep_by_index throws an exception, catch it and return -2.
+            (output the error message with the view).
+        */
+        
+        if($line == "exit" || $line == "q"){
+            return -1;
+        }
+        if($line == "" || $line == "none" || $line == "pass"){
+            return -2;
+        }
+        if($line == "all"){
+           $this->get_model()->combine_dice();
+           return 0;
+        }
+        try{
+          $result =  $this->get_model()->keep_by_index($line);
+        }
+        catch(Exception $result){
+          $this->view->output(strval($result));
+          return -2;
+         }
+          return $result;
+      
+       
     }
+    
+    public function handle_roll() : int {
+        /* 
+        handle_roll() : int
+        This method rolls the dice, up to three times, and gathers input from
+        the user on what to do with each roll.  This method should:
+        - Enter a loop that runs twice, and set a variable to track
+          $remaining_dice to 5.
+        - Each loop:
+          - Roll dice, the amount being whats in $remaining_dice. 
+          - Take the user input, feed it into process_keep_input()
+            > If the return value is -2, nothing happens, continue the loop.
+            > If the return value is -1, return -1.
+            > else, set the $remaining_dice local var to the return value.
+          > If the $remaining_dice is 0, break out of the loop.
+        > After the loop, if the model's kept_dice has less than 5 dice,
+          roll a final time, and then combine dice the kept_dice should be 
+          correct based on the rolls and inputs.
+        > Return 0.
+          
+        Hint for this one: What you can test from the result of this method is
+        that 1) the kept_dice is properly populated given a variety of
+        scenarios (simulate different inputs) and 2) the return value is
+        correct.  You'll want to stub the view to return pre-scripted
+        responses, a good way to do this is a stub's onConsecutiveCalls method.
+        */
+      $remaining_dice = 5;
+     
+      for($i = 0; $i < 2; $i++){
+       
+        $this->get_model()->roll($remaining_dice);
+        $inputValue = $this->get_view()->get_user_input("test");
+       
+        $result = $this->process_keep_input($inputValue);
+        if($result == -2){}
+        elseif($result == -1){
+          return -1;
+        }
+        else{
+          return $remaining_dice;
+        }
+        if($remaining_dice == 0){
+          break;
+        }
+      }
+      $diceNum = array_sum($this->get_model()->get_kept_dice());
+      if($diceNum < 5){
+        $this->get_model()->roll($diceNum);
+        $this->get_model()->combine_dice();
+      }
+
+        
+        return 0;
+    }
+    
+    public function main_loop() : int {
+        /*
+        main_loop() : int
+        Runs the main loop of the controller.
+        > The loop should run exactly 13 times, and the model's current_turn
+        should be incremented each time.
+        - (Output the current turn with the view)
+        > If handle_roll returns -1, return -1.
+        - (Output the kept dice after handle_roll with the view)
+        - Get the valid categories calling get_possible_categories
+        - (Output valid categories with view)
+        - (Get user input with view)
+        > Call process_score_input on the result from the user input.
+          > If the returning values is -1, return -1.
+        - (use the view to output the score)
+        > At the end of each loop iteration, clear_kept_dice and increment_turn
+        
+        Hint: For this, you can inspect the model to see if behavior is being
+        properly called/implemented.  You will also need to stub view to give
+        pre-scripted inputs.  Remember, we primarily want to test data and
+        behavior, NOT implementation details.
+        */
+
+        for($i = 0; $i < 13; $i++){
+          $rollValue = $this->handle_roll();
+          if($rollValue == -1){
+            return -1;
+          }
+          $availableCategories = $this->get_possible_categories();
+          $userInput = $this->get_view()->get_user_input("test");
+          $keptInputValue = $this->process_keep_input("$userInput");
+          if($keptInputValue == -1){
+            return -1;
+          }
+          $this->get_model()->clear_kept_dice();
+          $this->get_model()->increment_turn();
+        }
+        
+        
+        return 0;
+        
+    }
+}
+
+
+?>
