43109cea2f888c5a7e71cde4b030693648c33807
George Paredes <119899081+paredesg27@users.noreply.github.com>
2023-02-20 20:12:55 -0500

    Update ControllerTest.php

--- lab3/ControllerTest.php
+++ lab3/ControllerTest.php
@@ -31,34 +31,23 @@ class ControllerTest extends TestCase
         $this->assertNotNull($result);
     }
     public function test_get_possible_categories_ones(){
-       $stub = $this->createStub(Yahtzee::class);
-        // Configure the stub.
-      $stub->method('get_kept_dice')->willReturn(array(1,1,1,1,1));
-        $dice = $stub->get_kept_dice();
-        $this->model->update_scorecard("ones", array_sum($dice));
-        $result = $this->sut->get_possible_categories();
-        
-        $this->assertEquals($result['ones'], 5);
+        $result = $this->sut->get_possible_categories(); 
+        $this->assertNull($result['ones']);
     }
-    public function test_get_possible_categories_three_of_a_kind(){
-       $stub = $this->createStub(Yahtzee::class);
-        // Configure the stub.
-      $stub->method('get_kept_dice')->willReturn(array(1,1,1,1,1));
-        $dice = $stub->get_kept_dice();
-        $this->model->update_scorecard("three_of_a_kind", array_sum($dice));
-        $result = $this->sut->get_possible_categories();
-        
-        $this->assertEquals($result['three_of_a_kind'], 5);
-    }
+    public function test_get_possible_categories_ones_not_null(){
+        //    $stub = $this->createStub(Yahtzee::class);
+        //     // Configure the stub.
+        //   $stub->method('get_kept_dice')->willReturn(array(1,1,1,1,1));
+        //     $dice = $stub->get_kept_dice();
+        //     $this->model->update_scorecard("ones", array_sum($dice));
+            $result = $this->sut->get_possible_categories();
+            
+            $this->assertNull($result['ones'], 5);
+        }
+        //@covers get_possible_categories
     public function test_get_possible_categories_yahtzee(){
-        $stub = $this->createStub(Yahtzee::class);
-        // Configure the stub.
-        $stub->method('get_kept_dice')->willReturn(array(1,1,1,1,1));
-        $dice = $stub->get_kept_dice();
-        $this->model->update_scorecard("yahtzee", array_sum($dice));
-        $result = $this->sut->get_possible_categories();
-        
-        $this->assertEquals($result['yahtzee'], 5);
+        $result = $this->sut->get_possible_categories();  
+        $this->assertNull($result['yahtzee']);
     }
     // @cover process_score_input
      public function test_process_score_input_q(){
@@ -99,7 +88,9 @@ class ControllerTest extends TestCase
         $result = $this->sut->process_score_input("aple");
         $this->assertEquals($result, -2);
     }
-     // @cover process_keep_input
+     /**
+    * @covers::process_keep_input()
+    */
     public function test_process_keep_input_exit() {
         //set up (done in fixture)
         
@@ -109,92 +100,140 @@ class ControllerTest extends TestCase
         $this->assertEquals(-1, $result);
     
     }
-    // @cover process_keep_input
+  /**
+    * @covers::process_keep_input()
+    */
     public function test_process_keep_input_q() {
         //set up (done in fixture)
         $result = $this->sut->process_keep_input("q");
         $this->assertEquals(-1, $result);
     }
-    // @cover process_keep_input
+   /**
+    * @covers::process_keep_input()
+    */
     public function test_process_keep_input_do_nothing(){
         //> If the line given is "none" "pass" or blank "", return -2.
         $result = $this->sut->process_keep_input("none");
         $this->assertEquals(-2, $result);
     }
-    // @cover process_keep_input
+   /**
+    * @covers::process_keep_input()
+    */
     public function test_process_keep_input_do_nothing1(){
         //> If the line given is "none" "pass" or blank "", return -2.
         $result = $this->sut->process_keep_input("pass");
         $this->assertEquals(-2, $result);
     }
-    // @cover process_keep_input
+   /**
+    * @covers::process_keep_input()
+    */
     public function test_process_keep_input_do_nothing2(){
         //> If the line given is "none" "pass" or blank "", return -2.
         $result = $this->sut->process_keep_input("");
         $this->assertEquals(-2, $result);
     }
-    // @cover process_keep_input
+   /**
+    * @covers::process_keep_input()
+    */
     public function test_process_keep_input_all(){
         $this->sut->get_model()->roll(5);
         $result = $this->sut->process_keep_input("all");
         $this->assertEquals(0, $result);
         $this->assertEquals(5, count($this->sut->get_model()->get_kept_dice()));
     }
-    // @cover process_keep_input
+    /**
+    * @covers::process_keep_input()
+    */
     public function test_process_keep_input_valid_index(){
              $this->sut->get_model()->roll(5);
             $result = $this->sut->process_keep_input("1");
             $this->assertEquals(4, $result);
         }
-        // @cover process_keep_input
+        /**
+    * @covers::process_keep_input()
+    */
     public function test_process_keep_input_throw_exception_invalid_index(){
         $result = $this->sut->process_keep_input("ons");
         $this->assertEquals(-2, $result);
     }
-    // @cover process_keep_input
+    /**
+    * @covers::process_keep_input()
+    */
     public function test_process_keep_input_throw_exception_too_many_dice(){
         $result = $this->sut->process_keep_input("1 2 3 4 5 6");
         $this->assertEquals(-2, $result);
     }
-    // @cover handle_roll
+    /**
+    * @covers::handle_roll()
+        */
    public function test_handle_roll(){
         $result = $this->sut->handle_roll();
         $this->assertEquals($result, 0);
     }
-      // @cover handle_roll
+     /**
+    * @covers::handle_roll()
+        */
     public function test_handle_roll_correct_user_strings(){
+        $this->model = $this->CreateStub(Yahtzee::class);
+        $this->sut = new YahtzeeController($this->model, $this->view);
         $stub = $this->view;
+        $stub->method('get_kept_dice')->willReturnOnConsecutiveCalls(array(1,1), array(3), array(5));
+        $stub->method('get_scorecard')->willReturn(array(
+            "ones" => NULL,
+            "twos" => NULL,
+            "threes" => NULL,
+            "fours" => NULL,
+            "fives" => NULL,
+            "sixes" => NULL,
+            "three_of_a_kind" => NULL,
+            "four_of_a_kind" => NULL,
+            "full_house" => NULL,
+            "small_straight" => NULL,
+            "large_straight" => NULL,
+            "chance" => NULL,
+            "yahtzee" => NULL
+        ));
         $stub->method('get_user_input')->willReturnOnConsecutiveCalls("ones","threes","fives");
         $result = $this->sut->handle_roll();
         $this->assertEquals($result, 0);
     }    
-     // @cover handle_roll
+     /**
+    * @covers::handle_roll()
+        */
     public function test_handle_roll_invalid_user_strings(){
         $stub = $this->view;
         $stub->method('get_user_input')->willReturn("ons");
         $result = $this->sut->handle_roll();
         $this->assertEquals($result, 0);
     }
-    // @cover handle_roll
+     /**
+    * @covers::handle_roll()
+        */
     public function test_handle_roll_q_user_string(){
         $stub = $this->view;
         $stub->method('get_user_input')->willReturn("q");
         $result = $this->sut->handle_roll();
         $this->assertEquals($result, -1);
     }
-         // @cover main_loop
+        /**
+    * @covers::main_loop()
+        */
     public function test_main_loop(){
         $result = $this->sut->main_loop();
         $this->assertEquals($result, 0);
     }
-     // @cover main_loop
+      /**
+    * @covers::main_loop()
+        */
     public function test_main_loop_roll_q(){
         $stub = $this->view;
         $stub->method('get_user_input')->willReturn("q");
         $result = $this->sut->main_loop();
         $this->assertEquals($result, -1);
     }
-     // @cover main_loop
+     /**
+    * @covers::main_loop()
+        */
     public function test_main_loop_user_input_q(){
         $stub = $this->view;
         $stub->method('get_user_input')->willReturnOnConsecutiveCalls("all","q");
